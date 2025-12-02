f3c0817e3a8db359f74e7c26ac339e51f61a5897
Malathi2311 <malathipandian49@gmail.com>
2025-12-02 22:29:52 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -12,4 +12,20 @@ const auth = (req, res, next)=>{
     }
 }
 
+export const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token)
+    return res.json({ success: false, message: "No token provided" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.id;
+    req.isAdmin = decoded.isAdmin;
+    next();
+  } catch (err) {
+    return res.json({ success: false, message: "Invalid token" });
+  }
+};
+
+
 export default auth;
